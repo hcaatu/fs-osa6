84b2db4f4d1356101f77d3031dac386371fa5300
hcaatu <aatu.hokkane@gmail.com>
2025-03-19 16:22:41 +0200

    6.13 done

--- redux-anecdotes/src/components/Notification.jsx
+++ redux-anecdotes/src/components/Notification.jsx
@@ -1,5 +1,4 @@
 import { useSelector } from 'react-redux'
-import { newNotification } from '../reducers/notificationReducer'
 
 const Notification = () => {
   const notification = useSelector(state => state.notification)
